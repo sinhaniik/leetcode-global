802feaa951661aed0fd29abd2fcf83e5f25bf05a
Nikhil Sinha <nikhilsinha198@gmail.com>
2023-08-28 22:47:48 +0530

    start home page

---
+++ src/components/problemTable/problemTable.tsx
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import {IoClose} from "react-icons/io5";
+// import {problems} from "@/components/testData/testData";
+// const ProblemTable = () => {
+//     return(
+//         <tbody className="text-white">
+//         {problems.map((doc,id)=> {
+//             const difficultyColor = doc.difficulty === "Easy" ? "text-color-green-s" : doc.difficulty === "Hard" ? "text-dark-yellow" : "text-dark-pink"
+//             return(
+//                 <tr className={`${id % 2 == 1 ? "bg-dark-layer-1": ""  }`} key={doc.id}>
+//                     <thead ClassName=" px-2">
+//
+//                     </thead>
+//                 </tr>
+//
+//             )
+//         })}
+//         </tbody>
+//     )
+// };
+// export default ProblemTable

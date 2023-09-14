91c97a314817fe88eac61c59ddf81c3a51c7613e
Nikhil Sinha <nikhilsinha198@gmail.com>
2023-09-14 20:28:32 +0530

    code editor hardcoded

--- src/components/playground/playground.tsx
+++ src/components/playground/playground.tsx
@@ -1,13 +1,27 @@
+"use client"
 import React from 'react';
 import NewNav from "@/components/playground/newNav/newNav";
+import Split from "react-split"
+import CodeMirror from '@uiw/react-codemirror';
+import {darcula} from "@uiw/codemirror-theme-darcula";
+import {javascript} from "@codemirror/lang-javascript";
 
 type Props = {};
 const PlayGround = (props: Props) => {
     return (
-        <div className="flex justify-between ">
-            <>
-                <NewNav />
-            </>
+        <div className="flex flex-col bg-dark-layer-1 relative overflow-x-hidden">
+            <NewNav/>
+            <Split className="h-[calc(100vh-94px)]" direction="vertical" sizes={[70, 30]} minSize={60}>
+                <div className="w-full overflow-auto">
+                    <CodeMirror
+                        value="const a = 1;"
+                        theme={darcula}
+                        extensions={[javascript()]}
+                        style={{fontSize: 16}}
+                    />
+                </div>
+                <div> test cases</div>
+            </Split>
         </div>
     );
 };
